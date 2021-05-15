e60957461b7778081e3a119e82f9c36e7f91f9e7
Kritika <24kritikasharma24@gmail.com>
2021-05-15 18:22:58 +0530

    finishing touch

--- src/Pokemax.js
+++ src/Pokemax.js
@@ -3,24 +3,24 @@ import Pokecard from './Pokecard'
 import './Pokemax.css'
 
 class Pokemax extends Component {
-  static defaultProps = {
-    pokemon: [
-      { id: 4, name: 'Charmander', type: 'fire', base_experience: 62 },
-      { id: 7, name: 'Squirtle', type: 'water', base_experience: 63 },
-      { id: 11, name: 'Metapod', type: 'bug', base_experience: 72 },
-      { id: 12, name: 'Butterfree', type: 'flying', base_experience: 178 },
-      { id: 25, name: 'Pikachu', type: 'electric', base_experience: 112 },
-      { id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95 },
-      { id: 94, name: 'Gengar', type: 'poison', base_experience: 225 },
-      { id: 133, name: 'Eevee', type: 'normal', base_experience: 65 },
-    ],
-  }
   render() {
+    let title
+    let winner = this.props.isWinner
+    if (this.props.isWinner) {
+      title = <h1 className='Pokemax-winner'>Winner :)</h1>
+    } else {
+      title = <h1 className='Pokemax-loser'>Loser :(</h1>
+    }
     return (
       <div className='Pokemax'>
-        <h1>Pokemax</h1>
-        <p>Total experience: {this.props.exp}</p>
-        <p>{this.props.isWinner ? 'WINNER!!' : 'LOSER!!'}</p>
+        {title}
+        <h4>
+          Total experience:{' '}
+          <span className={winner ? 'Pokemax-winner' : 'Pokemax-loser'}>
+            {this.props.exp}
+          </span>
+        </h4>
+
         <div className='Pokemax-cards'>
           {this.props.pokemon.map((p) => (
             <Pokecard
