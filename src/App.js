5566716c3073217c0ca1f75ef4c199f066944f5f
Kritika <24kritikasharma24@gmail.com>
2021-05-15 17:05:36 +0530

    set up for main pokegame having 2 pokemax

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import Pokemax from './Pokemax'
+import Pokegame from './Pokegame'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Pokemax />
+        <Pokegame />
       </div>
     )
   }
